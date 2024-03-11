6e2c423ef4d6fd2c8d0747f06010a46ac0105588
ekansh2004 <ekansh.garg.1804@gmail.com>
2024-03-11 20:13:43 +0530

    v16

---
+++ Stacks/nextGreatestElementToRight.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class nextGreatestElementToRight {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number of Elements = ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        int[] nge = new int[n];
+        for(int i = 0; i < n; i++) {
+            System.out.print("Enter element at index " + i + " = ");
+            arr[i] = sc.nextInt();
+        }
+        sc.close();
+
+        Stack<Integer> st = new Stack<>();
+
+        st.push(arr[arr.length - 1]);
+        nge[arr.length - 1] = -1;
+        for(int i = arr.length - 2; i >= 0; i--){
+            //-a+
+            while(st.size() > 0 && arr[i] >= st.peek()){
+                st.pop();
+            }
+            if(st.size() == 0){
+                nge[i] = -1;
+            } else{
+                nge[i] = st.peek();
+            }
+            st.push(arr[i]);
+        }
+
+        System.out.println();
+        System.out.print("The Next Greatest Element to the right is = ");
+        for(int i = 0; i < nge.length; i++){
+            System.out.print(nge[i] + " ");
+        }
+        System.out.println();
+    }
+}
